6e8af71025c395245cdca7cd76058fd3e9ab4943
MaximProsh <karmark@yandex.ru>
2023-05-01 18:22:22 +0300

    first commit

---
+++ HW/ListNode.java
@@ -0,0 +1,44 @@
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        System.out.print("Изначальный список: ");
+        printList(head);
+
+        ListNode reversedHead = reverseList(head);
+
+        System.out.print("Перевернутый список: ");
+        printList(reversedHead);
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+}
